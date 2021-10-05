08e88aa20afeba6f0e90f480121d80bde7de7bad
ititiu20326-Trinh <91961753+ititiu20326-Trinh@users.noreply.github.com>
2021-10-05 21:10:41 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -13,9 +13,14 @@ Ex:
 #include <math.h>
 
 int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	int testcase = atoi(argv[1]);
-	//Your codes here
-	
-	return 0;
+    //testing variable, applying it to your algorithm for auto-evaluating
+    int testcase = atoi(argv[1]);
+    int digit;
+    while (testcase!=0)
+    {
+        digit = testcase % 10;
+        printf("%d ",digit);
+        testcase = (testcase-digit)/10;
+    }
+return 0;
 }
